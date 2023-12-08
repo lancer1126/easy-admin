36238341be395daef59d63de30cf6b63ec5202c0
lancer1126 <lancer1582@gmail.com>
2023-12-08 23:50:16 +0800

    easy-admin init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  bracketSpacing: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  trailingComma: "none"
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+Vue3 + Vite + TypeScript + ElementPlus  
+从Vue-pure-admin项目中学习前端知识
